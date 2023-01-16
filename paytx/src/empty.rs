30d4f5b7a726ce62ad561172a4c6ad936e5fe319
Florin <florinmihu31@gmail.com>
2023-01-16 20:04:51 +0200

    Use the payment ID and the amount as unique keys

--- paytx/src/empty.rs
+++ paytx/src/empty.rs
@@ -8,61 +8,41 @@ pub trait PayTx {
     fn init(&self) {}
 
     #[endpoint]
-    fn register_payment(&self, merchant_id: u32, payment_id: u32, payment_amount: BigUint) {
-        // Merchant ID must be greater than 0
-        require!(merchant_id > 0, "Merchant ID must be greater than 0");
-
+    fn register_payment(&self, payment_id: u32, payment_amount: BigUint) {
         // Payment ID must be greater than 0
         require!(payment_id > 0, "Payment ID must be greater than 0");
         
         // Payment amount must be greater than 0
         require!(payment_amount > 0, "Payment amount must be greater than 0");
 
-        // Check if the payment ID already exists
-        require!(self.payments(merchant_id).get(&payment_id).is_none(), "Payment ID already exists");
+        let payment_account_storage = self.payments(payment_id, payment_amount);
 
-        // Add the address corresponding to the merchant ID
-        if self.payment_account(merchant_id).is_empty() {
-            self.payment_account(merchant_id).set(&self.blockchain().get_caller());
-        } else {
-            // Check if the address corresponding to the merchant ID is the caller's address
-            require!(self.payment_account(merchant_id).get() == self.blockchain().get_caller(), "Caller is not the merchant");
-        }
+        // Check if the payment details already exist
+        require!(payment_account_storage.is_empty(), "Payment details already exist");
 
-        // Insert payment into the payments map corresponding to the merchant ID
-        self.payments(merchant_id).insert(payment_id, payment_amount);
+        // Insert the payment account into the storage
+        payment_account_storage.set(&self.blockchain().get_caller());
     }
 
     #[endpoint]
     #[payable("EGLD")]
-    fn pay(&self, merchant_id: u32, payment_id: u32) {
-        // Merchant ID must be greater than 0
-        require!(merchant_id > 0, "Merchant ID must be greater than 0");
-
+    fn pay(&self, payment_id: u32) {
         // Payment ID must be greater than 0
         require!(payment_id > 0, "Payment ID must be greater than 0");
 
-        // Get payment amount from payments map
-        let payment_amount = self.payments(merchant_id).get(&payment_id).unwrap_or_default();
+        let payment_account_storage = self.payments(payment_id, self.call_value().egld_value());
 
-        // Check if payment_amount is equal to the amount sent
-        require!(payment_amount != 0, "Payment ID does not exist");
-        
-        // Check if payment_amount is equal to the amount sent
-        require!(payment_amount == self.call_value().egld_value(), "Payment amount does not match");
+        // Check if the storage is not empty
+        require! (!payment_account_storage.is_empty(), "No payment details found");
 
-        // Send from caller to payment account payment_amount EGLD
-        self.send().direct_egld(&self.payment_account(merchant_id).get(), &self.call_value().egld_value());
+        // Send the payment to the merchant
+        self.send().direct_egld(&payment_account_storage.get(), &self.call_value().egld_value());
 
-        // Remove payment from payments map
-        self.payments(merchant_id).remove(&payment_id);
+        // Remove the payment from the storage
+        payment_account_storage.clear();
     }
 
-    // Map with payment ID as key and payment as value for each merchant
-    #[storage_mapper("payments")]
-    fn payments(&self, merchant_id: u32) -> MapMapper<u32, BigUint>;
-
     // Payment account hash of the merchant
-    #[storage_mapper("payment_account")]
-    fn payment_account(&self, merchant_id: u32) -> SingleValueMapper<ManagedAddress>;
+    #[storage_mapper("payments")]
+    fn payments(&self, payment_id: u32, payment_amount: BigUint) -> SingleValueMapper<ManagedAddress>;
 }
